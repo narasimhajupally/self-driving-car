5372195c940965bab978db26e53492be7136dc6f
Jupally <narasimhajupally@gmail.com>
2022-05-20 01:08:46 +0530

    ray casting

--- car.js
+++ car.js
@@ -12,11 +12,13 @@ class Car{
         this.friction = 0.05;
         this.angle = 0;
 
+        this.sensor = new Sensor(this);
         this.controls = new Controls();
     }
 
-    update() {
+    update(roadBorders) {
         this.#move();
+        this.sensor.update(roadBorders);
     }
     #move() {
         if (this.controls.forward) {
@@ -51,11 +53,15 @@ class Car{
     }
 
     draw(ctx) {
+        ctx.save();
         ctx.translate(this.x, this.y);
         ctx.rotate(-this.angle);
 
         ctx.beginPath();
         ctx.rect(- this.width / 2, - this.height / 2, this.width, this.height);
         ctx.fill();
+        ctx.restore();
+
+        this.sensor.draw(ctx);
     }
 }
--- utils.js
+++ utils.js
@@ -1,3 +1,7 @@
 function lerp(A, B, t) {
     return A + (B - A) * t;
 }
+
+function getIntersection() {
+    return null;
+}
